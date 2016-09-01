6d08cdf450c5a49595cc00cb115e157da8d35616
Shiv M <shiv@netherbox.com>
2016-08-31 22:48:46 -0500

    Added sponsorships field

--- support.php
+++ support.php
@@ -19,6 +19,10 @@ require_once('structure/header.php');
 			<p><a href="https://billing.netherbox.com/submitticket.php?step=2&amp;deptid=1">Pre-Sales Questions</a></p>
 			<a href="https://billing.netherbox.com/submitticket.php?step=2&amp;deptid=1" class="bttn">Select</a>
 		</article>
+		<article>
+			<p><a href="https://billing.netherbox.com/submitticket.php?step=2&amp;deptid=6">Sponsorships</a></p>
+			<a href="https://billing.netherbox.com/submitticket.php?step=2&amp;deptid=6" class="bttn">Select</a>
+		</article>
 	</section>
 	<section>
 		<h4><span>If you are an <u>EXISTING</u> client</span></h4>
