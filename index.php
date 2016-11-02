3179aeb91258c9010431e4538c64f812c69cb5e9
Shiv M <shiv@netherbox.com>
2016-11-02 18:08:59 -0500

    Bump 0.15.10 to 0.16

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $page = [
 
 	// These versions are for the "start trial" buttons in the header
 	'version_pc'	=> '1.10.2',
-	'version_pe'	=> '0.15.10',
+	'version_pe'	=> '0.16.0',
 ];
 
 require_once('structure/header.php');
